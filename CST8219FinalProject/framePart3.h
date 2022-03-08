83754fca01d6fa88206d956847e15fa5a901c883
Megan Machkouri <mach0079@algonquinlive.com>
2022-03-08 10:53:56 -0500

    First Commit

---
+++ CST8219FinalProject/framePart3.h
@@ -0,0 +1,14 @@
+//Frame.h (Part 3)//@author: Surbhi Bahri
+ 
+#pragma once
+Class frame
+{
+char* frameName;
+double duration;
+public:
+frame(char*, double);
+frame(const frame&);
+~frame();
+frame& operator=(const Frame&);
+friend std::ostream& operator << (std::ostream&, frame&);
+};
